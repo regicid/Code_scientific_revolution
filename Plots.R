b4a9d1761f0be3c9eeae538366509d632fbe1d93
regicid <57593826+regicid@users.noreply.github.com>
2021-05-30 22:51:11 +0200

    bla

--- Plots.R
+++ Plots.R
@@ -1,5 +1,10 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+
 setwd("~/Code_scientific_revolution")
-Data = read.csv("data.csv") 
+Data = read.csv("data_scientists.csv") 
+Population = read.csv("Population.csv")
 Countries<-c("Netherlands","France","Germany","United Kingdom","Belgium","Russia","Iberia","Italy","Poland","Scandinavia")
 index = function(Data,start=1300,end=1850,countries=Countries,per_capita=TRUE,gap=10){
   Results = data.frame(matrix(ncol=3,nrow=0))
@@ -12,7 +17,7 @@ index = function(Data,start=1300,end=1850,countries=Countries,per_capita=TRUE,ga
       Results[n+1,"Date"] = Dates[i]
       Results[n+1,"Countries"] = countries[j]
       #Results[n+1,"gdp"] = GDP[countries[j],as.character(Dates[i])]
-      m = which(Data$Countries == countries[j] & Data$Birth_dates %in% (Dates[i]-85):(Dates[i]-15))
+      m = which(Data$Countries == countries[j] & Data$Birth_dates %in% (Dates[i]-60):(Dates[i]-11))
       if(per_capita){Results[n+1,"Scientific_production"] = sum(Data[m,"pca"])/Population[countries[j],as.character(Dates[i])] 
       }else{Results[n+1,"Scientific_production"] = sum(Data[m,"pca"])}
     }
@@ -22,13 +27,14 @@ index = function(Data,start=1300,end=1850,countries=Countries,per_capita=TRUE,ga
 }
   
   
-plot = function(Results){
+plot_results = function(Results){
   ggplot(Results,aes(Date,Scientific_production,col=Countries)) +
     geom_point(size=1,alpha=0.4,aes(col=Countries)) + 
-    geom_smooth(span=0.8,se = FALSE) + theme_classic() +  
+    geom_smooth(span=0.7,se = FALSE) + theme_classic() +  
     theme(legend.title = element_blank(),axis.ticks = element_line(colour = "grey")) + 
     guides(color=guide_legend(override.aes=list(fill=NA))) + ylab("") + xlab("") + 
-    scale_x_continuous(expand=c(0,0)) + theme(plot.title = element_text(hjust = 0.5))
+    scale_x_continuous(expand=c(0,0)) + theme(plot.title = element_text(hjust = 0.5)) +
+    scale_y_continuous(limit=c(0,NA),oob=squish)
   
 }
 
@@ -36,47 +42,64 @@ plot = function(Results){
 Results = index(Data,1500)
 a = ggplot(Results,aes(Date,Scientific_production,col=Countries)) +
   geom_point(size=1,alpha=0.4,aes(col=Countries)) + 
-  geom_smooth(span=0.8,se = FALSE) + theme_classic() +  
+  geom_smooth(span=0.7,se = FALSE) + theme_classic() +  
   theme(legend.title = element_blank(),axis.ticks = element_line(colour = "grey")) + 
   guides(color=guide_legend(override.aes=list(fill=NA))) + ylab("") + xlab("") + 
   scale_x_continuous(expand=c(0,0)) + theme(plot.title = element_text(hjust = 0.5)) + 
   geom_rect(data=prosp,xmin=c(1550,1700),xmax=c(1700,1850),ymin=-Inf,ymax=+Inf,inherit.aes=F,fill=c("red","blue"),alpha=0.1) + 
   annotate("text",x=1770,y=4.2,label = "UK prosperity",size=5) + 
-  annotate("text",x=1615,y=4.2,label = "Netherlands prosperity",size=5) + 
+  annotate("text",x=1625,y=4.2,label = "Netherlands prosperity",size=5) + 
   #annotate("text",x=1625,y=3.8,label = "prosperity",size=5) + 
   theme(plot.title = element_text(size=22)) + 
-  theme(axis.text.x = element_text(size=11),axis.text.y = element_text(size=11))
+  theme(axis.text.x = element_text(size=11),axis.text.y = element_text(size=11)) + scale_y_continuous(limit=c(0,NA),oob=squish)
 print(a)
-ggsave("BBS.png")
+ggsave("plots/BBS.png")
 
 
 Data$rank = rank(Data$pca)
 Results = index(filter(Data,rank>nrow(Data)*.9),1500)
-a = ggplot(dplyr::filter(Results,Date>=1500),aes(Date,Scientific_production,col=Countries)) +
-  geom_point(size=1,alpha=0.4,aes(col=Countries)) + 
-  geom_smooth(span=0.8,se = FALSE) + theme_classic() +  
-  theme(legend.title = element_blank(),axis.ticks = element_line(colour = "grey")) + 
-  guides(color=guide_legend(override.aes=list(fill=NA))) + ylab("") + xlab("") + 
-  scale_x_continuous(expand=c(0,0)) + theme(plot.title = element_text(hjust = 0.5))
-print(a)
+plot_results(Results)
 Results = index(filter(Data,rank<nrow(Data)*.9),1500)
-a = plot(Results)
+a = plot_results(Results)
 print(a)
 
 Data_north = Data
 z = which(Data$Countries %in% c("Belgium","Netherlands","United Kingdom"))
 Data_north$Countries[z] = "Northwestern Europe"
 Results = index(Data_north,1500,countries = c("Northwestern Europe","France","Italy","Germany","Scandinavia","Iberia","Poland","Russia"))
-plot(Results)
+plot_results(Results)
 
 Results = index(Data,1500,per_capita = FALSE)
-a = plot(Results)
-ggsave(a,"Results_brut")
+a = plot_results(Results)
+ggsave("plots/Results_brut.png")
 
-Data$century = as.character(Data$Birth_dates+100) 
+Results = index(filter(Data,Occupation=="scientist"),1500)
+a = plot_results(Results)
+ggsave("plots/Results_scientists_only.png")
+
+Data$century = as.character(as.numeric(Data$Birth_dates)+100)
 Data$century = substring(Data$century,1,2)
 Data$century = paste(Data$century,"th-century",sep="")
 a = ggplot(filter(Data,Birth_dates %in% 1500:1799),aes(Countries)) + geom_bar() + 
   facet_grid(~century,scales="free_x") + theme_minimal() + coord_flip() +
   theme(axis.ticks = element_blank(),strip.background = element_blank(),panel.grid.minor.y =element_blank(),panel.grid.major.y =element_blank(),panel.grid.minor = element_blank(),panel.grid.major = element_blank()) + ylab("Scientists numbers")
-print(a)
+ggsave("plots/Hist.png")
+
+Data_cultural = read.csv("data_cultural.csv")
+Results = index(Data = Data_cultural,1500)
+plot_results(Results)
+Data = read.csv("data_scientists.csv")
+Results2 = index(Data,1500)
+plot_results(Results)
+
+Protestant = c("Germany","Scandinavia","Netherlands","United Kingdom")
+Catholic = c("France","Italy","Iberia","Poland","Belgium")
+Data_protestant = Data
+z = which(Data$Countries %in% Protestant)
+Data_protestant$Countries[z] = "Protestant countries"
+z = which(Data$Countries %in% Catholic)
+Data_protestant$Countries[z] = "Catholic countries"
+Population["Catholic countries",] = colSums(Population[Catholic,])
+Population["Protestant countries",] = colSums(Population[Protestant,])
+Results = index(Data_protestant,countries = c("Protestant countries","Catholic countries"))
+plot_results(Results)
---
+++ Pomp.R
@@ -0,0 +1,156 @@
+library(tidyr)
+library(panelPomp)
+library(reshape)
+library(rslurm)
+library(doParallel)
+library(dplyr)
+library(foreach)
+analysis = "MLE_with_sigma2"
+options =  list("dellgen","benoit2c@gmail.com","ALL")
+names(options) = c("partition","mail-user","mail-type")
+pkg = c("panelPomp")
+cpus = c(12,12,16,16,40,48)
+names(cpus) = c("firstgen","fastgen","secondgen","lastgen","dellgen","gpu")
+#partitions = c(rep("dellgen",7),"gnt")
+##Importations
+Countries<-c("Italy","France","Poland","Germany","United Kingdom","Iberia","Russia","Belgium","Netherlands","Scandinavia")
+Results = read.csv("Results.csv",row.names = 1)
+Results$Nobs = log(Results$Nobs+1)
+Results$Nobs = Results$Nobs/sd(Results$Nobs)
+Results$gdp = Results$gdp - min(Results$gdp)
+Distances = read.csv("Distances.csv",row.names = 1)
+
+#Create the diffusion matrix
+R = Results[c("Countries","Date","Nobs")]
+R  = spread(R,"Countries","Nobs")
+rownames(R) = R$Date
+R = R[-1]
+R = R[Countries]
+
+Csnippet("
+         N = N_0;
+         ") -> rinit
+Csnippet("
+         double eps_obs = rnorm(0,pow(sigma_obs,2));
+         Nobs = N+eps_obs;
+         ") -> rmeas
+Csnippet("
+         lik = dnorm(Nobs,N,pow(sigma_obs,2),give_log);
+         ") -> dmeas
+Csnippet("double eps = fmax(rnorm(1,pow(sigma,2)),0);
+         double eps2 = rnorm(0,pow(sigma2,2));
+         N = z*N*eps + e*cum + f*gdp*cum + a*gdp  + d*diff + b*gdp*diff + g*cum_diff + h*protestant + c + eps2;
+         ") -> evol_diff
+
+
+
+PARAM = c("bla","a","b","c","d","e","f","z","sigma","sigma_obs","N_0","sigma2","g","h")
+job = list()
+job2 = list()
+mifs_pomp = list()
+unused_parameters = list()
+names = c("A","B","C","D",'E','F','G','H','I','J','K','L')
+mifs_pomp = list()
+unused_parameters = list()
+unused_parameters[[1]] = c(1,2,3,5,6,7,8,13,14)
+unused_parameters[[2]] = c(1,2,3,5,6,7,13,14)
+unused_parameters[[3]] = c(1,3,5,6,7,13,14)
+unused_parameters[[4]] = c(1,2,3,5,7,13,14)
+unused_parameters[[5]] = c(1,3,5,6,13,14)
+unused_parameters[[6]] = c(1,2,3,6,7,13,14)
+unused_parameters[[7]] = c(1,3,6,7,13,14)
+unused_parameters[[8]] = c(1,2,5,6,7,13,14)
+unused_parameters[[9]] = c(1,5,6,7,13,14)
+unused_parameters[[10]] = c(1,2,3,5,6,7,14)
+unused_parameters[[11]] = c(1)
+unused_parameters[[12]] = c(1,3,5,6,7,13)
+names(unused_parameters) = names
+
+unused_parameters = list(unused_parameters[[12]])
+names(unused_parameters) = "L"
+
+
+submit_job <- function(nmif=2,np=2,
+                       cooling_fraction=.95,n=1){
+  Pomps = list()
+  for(country in Countries){
+    data = dplyr::filter(Results,Countries==country)
+    t0=1300
+    if(country=="Russia"){
+      data = dplyr::filter(data,Date>=1500)
+      t0=1500
+    }
+    z = which(!is.na(data$gdp))
+    Gdp = data[,c("Date","gdp","protestant")]
+    Gdp$diff = rowSums(R/untable(Distances[country,],12)**2)[as.character(data$Date)]
+    data = data[c("Date","Nobs")]
+    Gdp$cum = vector("numeric",length = nrow(Gdp))
+    Gdp$cum[2:nrow(Gdp)] = cumsum(data$Nobs)[1:nrow(Gdp)-1]
+    Gdp$cum_diff = cumsum(Gdp$diff)
+    
+    data %>%
+      pomp(
+        times="Date", t0=t0,rinit=rinit,
+        covar = covariate_table(Gdp, times= "Date"),
+        rprocess=discrete_time(evol_diff,delta.t = 50),
+        dmeasure = dmeas,obsnames = c("Nobs"),
+        statenames=c("N"),rmeasure=rmeas,
+        paramnames=PARAM[-1],covarnames = c("gdp","protestant","diff","cum","cum_diff")
+      ) -> Pomps[[country]]}
+  p = rep(0,length(PARAM[-1]))
+  names(p) = PARAM[-1]
+  Model_diff = panelPomp(Pomps,shared = p)
+  lower = c(a=.05,b=-.5,c=0,d=-.5,e=-.2,f=-.1,z=1,sigma=.8,sigma_obs=.07,N_0 = -.1,sigma2=.1,g=-.1,h=-.1)
+  upper = c(a=.1,b=.5,c=0,d=.5,e=.2,f=.1,z=1.3,sigma=1.1,sigma_obs=.15,N_0 = .1,sigma2=.3,g=-.1,h=.1)
+  lower[unused_parameters[[name]]-1] = 0
+  upper[unused_parameters[[name]]-1] = 0
+  sobolDesign(lower = lower[PARAM[-1]], upper = upper[PARAM[-1]], nseq = n) -> guesses
+  rwsd = rw.sd(a=.1,b=.1,c=.1,d=.1,e=.1,f=.1,z=.2,sigma=.15,sigma_obs=.15,N_0 = ivp(.1),sigma2=.1,g=.1,h=.1)
+  rwsd@call[PARAM[unused_parameters[[name]]][-1]] = 0
+  Model_diff %>%
+    panelPomp::mif2(
+      shared.start=unlist(guesses[6,]),
+      specific.start = Model_diff@specific,
+      Np=np,
+      Nmif=3,
+      cooling.fraction.50=cooling_fraction,
+      cooling.type="geometric",
+      rw.sd= rwsd,
+      pars = PARAM
+    ) -> mf1
+  mif3 <- function(a,sigma,N_0,sigma_obs,z,d,c,b,e,f,sigma2,g,h){
+    k = panelPomp::mif2(mf1,Nmif = nmif,shared = c(a = a,sigma = sigma,N_0 = N_0,sigma_obs = sigma_obs,z = z,d = d,b=b,c=c,e=e,f=f,sigma2=sigma2,g=g,h=h), specific = Model_diff@specific)
+    return(k)
+  }
+  
+  k = slurm_apply(f = mif3,params = guesses,jobname = paste(name,analysis,sep="_"),nodes = 15,cpus_per_node = cpus[options[[1]]],pkgs = pkg,slurm_options = options,add_objects = c("mf1","Model_diff"))
+  return(k)
+}
+
+for(name in names){
+  job[[name]] = submit_job()
+}
+
+for(name in names){
+  mifs_pomp[[name]] = get_slurm_out(job[[name]],wait = TRUE)
+  #cleanup_files(job[[name]])
+  file1 = paste(paste("mifs_pomp_",analysis,sep="_"),name,sep="")
+  file1 = paste(file1, ".RDS",sep="")
+  saveRDS(mifs_pomp[[name]],file1)
+}
+
+
+for(name in names){
+  mifs = mifs_pomp[[name]]
+  d = data.frame(i = 1:length(mifs))
+  job2[[name]] = slurm_apply(f = eval,params = d,jobname = paste(paste("ev",name,sep="_"),analysis,sep="_"),nodes = 15,cpus_per_node = cpus[options[[1]]],pkgs = pkg,slurm_options = options,add_objects = c("mifs"))
+}
+
+for(name in names){
+  estimates = get_slurm_out(job2[[name]], outtype = "table",wait=TRUE)
+  #cleanup_files(job2[[name]])
+  file2 = paste(paste("estimates_",analysis,sep="_"),name,sep="")
+  file2 = paste(file2, ".csv",sep="")
+  write.csv(estimates,file2)
+}
+
